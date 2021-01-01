698b2b45f28335c5b57e36d687e20bf59c1d194f
Axel Nana <axel.nana@aliens-group.com>
2021-01-01 16:10:40 +0100

    ♻️ Refactor algorithm of repeat nodes

--- src/RepeatUntilFailureNode.lua
+++ src/RepeatUntilFailureNode.lua
@@ -7,30 +7,40 @@ local DecoratorNode = require("src.DecoratorNode");
 ---@field maxCount number The maximum allowed number of iterations.
 local RepeatUntilFailureNode = class('RepeatUntilFailureNode', DecoratorNode);
 
+function RepeatUntilFailureNode:initialize(config)
+    DecoratorNode.initialize(self, config);
+    -- The maxCount defaults to an infinite loop.
+    self.maxCount = -1;
+end
+
 function RepeatUntilFailureNode:start()
     DecoratorNode.start(self);
-    self._count = 0;
+    self._count = 1;
 end
 
 function RepeatUntilFailureNode:success()
     if self.maxCount > -1 and self._count == self.maxCount then
+        self._count = 1;
         self._parent:failure();
-        self._count = 0;
     else
-        self._parent:running();
         self._count = self._count + 1;
+        self._parent:running();
     end
 end
 
 function RepeatUntilFailureNode:failure()
+    self._count = 1;
     self._parent:success();
-    self._count = 0;
 end
 
 --- Updates the `maxCount` value of this node.
 ---@param maxCount number The maximum number of iterations.
 function RepeatUntilFailureNode:setMaxCount(maxCount)
-    self.maxCount = maxCount;
+    if type(maxCount) == "string" then
+        self.maxCount = tonumber(maxCount);
+    elseif type(maxCount) == "number" then
+        self.maxCount = maxCount;
+    end
 end
 
 return RepeatUntilFailureNode;
--- src/RepeatUntilSuccessNode.lua
+++ src/RepeatUntilSuccessNode.lua
@@ -7,30 +7,40 @@ local DecoratorNode = require("src.DecoratorNode");
 ---@field maxCount number The maximum allowed number of iterations.
 local RepeatUntilSuccessNode = class('RepeatUntilSuccessNode', DecoratorNode);
 
+function RepeatUntilSuccessNode:initialize(config)
+    DecoratorNode.initialize(self, config);
+    -- The maxCount defaults to an infinite loop.
+    self.maxCount = -1;
+end
+
 function RepeatUntilSuccessNode:start()
     DecoratorNode.start(self);
-    self._count = 0;
+    self._count = 1;
 end
 
 function RepeatUntilSuccessNode:failure()
     if self.maxCount > -1 and self._count == self.maxCount then
+        self._count = 1;
         self._parent:failure();
-        self._count = 0;
     else
-        self._parent:running();
         self._count = self._count + 1;
+        self._parent:running();
     end
 end
 
 function RepeatUntilSuccessNode:success()
-    self._count = 0;
+    self._count = 1;
     self._parent:success();
 end
 
 --- Updates the `maxCount` value of this node.
 ---@param maxCount number The maximum number of iterations.
 function RepeatUntilSuccessNode:setMaxCount(maxCount)
-    self.maxCount = maxCount;
+    if type(maxCount) == "string" then
+        self.maxCount = tonumber(maxCount);
+    elseif type(maxCount) == "number" then
+        self.maxCount = maxCount;
+    end
 end
 
 return RepeatUntilSuccessNode;

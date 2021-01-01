73a47c2d1619cf0313c39d25e082be0ddf1819d6
Axel Nana <axel.nana@aliens-group.com>
2021-01-01 16:07:23 +0100

    💬 Fix doc comment

--- src/ReactiveSequence.lua
+++ src/ReactiveSequence.lua
@@ -2,9 +2,9 @@ local class = require('src.Utils.MiddleClass');
 
 local Sequence = require('src.Sequence');
 
---- Executes each child nodes sequentially until one node returns `fail()`,
+--- Executes each child nodes sequentially until one node returns `failure()`,
 --- or all nodes returns `success()`. If every child nodes returns `success()`,
---- the `ReactiveSequence` node returns `success()` too, otherwise returns `fail()`.
+--- the `ReactiveSequence` node returns `success()` too, otherwise returns `failure()`.
 --- If a node returns `running()`, the `ReactiveSequence` node will restart the sequence.
 ---@class ReactiveSequence: Sequence
 local ReactiveSequence = class('ReactiveSequence', Sequence);
